f1a3c499f511093f423c3c7a9903dc165bb4f1ef
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-10-10 15:57:26 +0530

    minor customization

--- resources/js/Layouts/Guest.js
+++ resources/js/Layouts/Guest.js
@@ -6,8 +6,9 @@ export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+                <Link href="/" className="flex flex-col items-center">
+                    <ApplicationLogo className="w-20 h-20" />
+                    <h1 className="text-2xl">ColoredCow Monitor</h1>
                 </Link>
             </div>
 
