06c857ee729222848e92eeaa95d61088e0426ec7
Vaibhav Rathore <vaibhav@coloredcow.com>
2024-05-11 19:37:31 +0530

    cleanups

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,6 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 Route::permanentRedirect('/', '/login');
 
 Route::get('/dashboard', function () {
